0c0cd469c8ef6fa75960745a35e6188e320a06a3
Adam Hall <adamhalldesigns@gmail.com>
2020-04-12 20:49:03 +0100

    abstract common fn

--- client/main.go
+++ client/main.go
@@ -22,36 +22,20 @@ func main() {
 	g := gin.Default()
 
 	g.GET("/add/:a/:b", func(ctx *gin.Context){
-		a, err := strconv.ParseUint(ctx.Param("a"), 10, 64)
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter A"})
-		}
-
-		b, err := strconv.ParseUint(ctx.Param("b"), 10, 64)
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter B"})
-		}
+		a := getParamOfFail(ctx, "a")
+		b := getParamOfFail(ctx, "b")
 
 		req := &proto.Request{A: int64(a), B: int64(b)}
 		if response, err := client.Add(ctx, req); err == nil {
-			ctx.JSON(http.StatusOK, gin.H{
-				"result": fmt.Sprint(response.Result),
-			})
+			ctx.JSON(http.StatusOK, gin.H{"result": fmt.Sprint(response.Result)})
 		} else {
 			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		}
 	})
 
 	g.GET("/mult/:a/:b", func(ctx *gin.Context){
-		a, err := strconv.ParseUint(ctx.Param("b"), 10, 64)
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter A"})
-		}
-
-		b, err := strconv.ParseUint(ctx.Param("b"), 10, 64)
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter B"})
-		}
+		a := getParamOfFail(ctx, "a")
+		b := getParamOfFail(ctx, "b")
 
 		req := &proto.Request{A: int64(a), B: int64(b)}
 
@@ -65,5 +49,12 @@ func main() {
 	if err := g.Run(":8080"); err != nil {
 		log.Fatalf("Failed to run Server: %v", err)
 	}
+}
 
+func getParamOfFail(ctx *gin.Context, key string) uint64 {
+	param, err := strconv.ParseUint(ctx.Param(key), 10, 64)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("Invalid Parameter %s", key)})
+	}
+	return param
 }
